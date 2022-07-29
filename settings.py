d593a73dbfd8f1ea0eeed818d19372ea889ddf75
SalmanAsh <102732552+SalmanAsh@users.noreply.github.com>
2022-07-29 11:11:40 +0100

    speeds changed

--- settings.py
+++ settings.py
@@ -13,10 +13,10 @@ class Settings:
         self.bg_color = (224, 238, 238)
 
         # ship settings
-        self.ship_speed = 3
+        self.ship_speed = 5
 
         # bullet settings
-        self.bullet_speed = 1.0
+        self.bullet_speed = 5.0
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
